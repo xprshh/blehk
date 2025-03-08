fc4b1a4b8f8d26b31e7dfa2ef7ed60f3e4ea4826
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-08 15:56:52 -0500

    Update rust-dev.nix

--- home-manager/scripts/rust-dev.nix
+++ home-manager/scripts/rust-dev.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   rust-dev = pkgs.writeShellScriptBin "rust-dev" ''
-   nix-shell -p  rustc cargo pkg-config gtk4 glib cairo gobject-introspection wrapGAppsHook
+   nix-shell -p  rustc cargo pkg-config gtk4 glib cairo gobject-introspection wrapGAppsHook gtk3 gobject-introspection
   '';
 in {
   home.packages = [rust-dev];
