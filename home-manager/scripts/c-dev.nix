fff98fcd9b3a5faca87e778156926bb38d793f5d
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-08 00:26:26 -0500

    Update c-dev.nix

--- home-manager/scripts/c-dev.nix
+++ home-manager/scripts/c-dev.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: let
-  rust-dev = pkgs.writeShellScriptBin "rust-dev" ''
+  c-dev = pkgs.writeShellScriptBin "c-dev" ''
    nix-shell -p gcc make cmake pkg-config
   '';
 in {
-  home.packages = [rust-dev];
+  home.packages = [c-dev];
 }
