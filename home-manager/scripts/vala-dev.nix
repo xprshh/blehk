6331c29f85ce12a7de848a83ce4f718839f82485
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-09 12:19:07 -0400

    Create vala-dev.nix

---
+++ home-manager/scripts/vala-dev.nix
@@ -0,0 +1,7 @@
+{pkgs, ...}: let
+  rust-dev = pkgs.writeShellScriptBin "vala-dev" ''
+   nix-shell -p gtk4 gtk3 vala meson ninja pkg-config gcc glib ligee gobject-introspection gtk-doc clang gtk-layer-shell clang 
+  '';
+in {
+  home.packages = [vala-dev];
+}
