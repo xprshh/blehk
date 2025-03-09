1297fcac16503be634e47ae4a2f80239aaf0ab4e
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-09 13:36:46 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
       inputs.hyprland.follows = "hyprland";
     };
 
-    matugen.url = "github:InioX/matugen?ref=v2.4.1";
+    matugen.url = "github:InioX/matugen?ref=v2.2.0";
     ags.url = "github:Aylur/ags";
     astal.url = "github:Aylur/astal";
 
