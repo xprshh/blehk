0e9812ee7515242e3b637a0664e4abf48dd1a480
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-09 14:00:45 -0400

    Update nx-clean.nix

--- home-manager/scripts/nx-clean.nix
+++ home-manager/scripts/nx-clean.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: let
   nx-clean = pkgs.writeShellScriptBin "nx-clean" ''
 sudo nix-env --delete-generations old
+sudo nix-collect-garbage -d
 sudo nix-store --gc
    sudo nix-store --optimise
   '';
