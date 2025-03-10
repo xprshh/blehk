a722be1519617ae2af45e92f536bd0943a0a58ea
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-10 17:47:07 -0400

    fix

--- home-manager/scripts/vala-dev.nix
+++ home-manager/scripts/vala-dev.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   vala-dev = pkgs.writeShellScriptBin "vala-dev" ''
-   nix-shell -p gtk4 gtk3 vala meson ninja pkg-config gcc glib ligee gobject-introspection gtk-doc clang gtk-layer-shell clang 
+   nix-shell -p gtk4 gtk3 vala meson ninja pkg-config gcc glib libgee gobject-introspection gtk-doc clang gtk-layer-shell clang 
   '';
 in {
   home.packages = [vala-dev];
